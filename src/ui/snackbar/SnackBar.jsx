8d685377a945bc903db548fbd27f66ed2c9335f5
Jeremy Bourqui <jbourqui@gmail.com>
2026-01-23 16:20:26 +0100

    feat(ui): refactor to extract types from components to align with other components structures and better performances, add animations

--- src/ui/snackbar/SnackBar.jsx
+++ src/ui/snackbar/SnackBar.jsx
@@ -4,87 +4,100 @@ import PropTypes from "prop-types";
 
 import Icon from "../icon/Icon";
 
+const snackBarTypes =
+{
+    "neutral": {
+        icon: null,
+        backgroundColor: "bg-white",
+        borderColor: "border-black",
+        color: "text-black"
+    },
+    "info": {
+        icon: "info",
+        backgroundColor: "bg-primary",
+        borderColor: "border-black",
+        color: "text-white"
+    },
+    "success": {
+        icon: "check",
+        backgroundColor: "bg-green-500/80",
+        borderColor: "border-black",
+        color: "text-black"
+    },
+    "warning": {
+        icon: "exclamation-triangle",
+        backgroundColor: "bg-amber-500/80",
+        borderColor: "border-black",
+        color: "text-black"
+    },
+    "error": {
+        icon: "exclamation-circle",
+        backgroundColor: "bg-red-500/80",
+        borderColor: "border-black",
+        color: "text-black"
+    }
+}
+
 const Snackbar = ({
     type = "neutral",
     message,
     autoCloseTimer = 5000,
     className
-}) =>
-{
-    const snackBarTypes =
-    {
-        "neutral": {
-            icon: null,
-            backgroundColor: "bg-white",
-            borderColor: "border-black",
-            color: "text-black"
-        },
-        "info": {
-            icon: "info",
-            backgroundColor: "bg-primary",
-            borderColor: "border-black",
-            color: "text-white"
-        },
-        "success": {
-            icon: "check",
-            backgroundColor: "bg-green-500/80",
-            borderColor: "border-black",
-            color: "text-black"
-        },
-        "warning": {
-            icon: "exclamation-circle",
-            backgroundColor: "bg-amber-500/80",
-            borderColor: "border-black",
-            color: "text-black"
-        },
-        "error": {
-            icon: "exclamation-triangle",
-            backgroundColor: "bg-red-500/80",
-            borderColor: "border-black",
-            color: "text-black"
-        }
-    }
+}) => {
 
     const [isDisplayed, setIsDisplayed] = useState(true);
+    const [isClosing, setIsClosing] = useState(false);
 
     // To display the time in a thin horizontal line.
     const [progress, setProgress] = useState(100);
     const intervalDelay = 50;
 
-    useEffect(() =>
-    {
+    const handleClose = () => {
+        setIsClosing(true);
+        setTimeout(() => setIsDisplayed(false), 300);
+    };
+
+    useEffect(() => {
         const start = Date.now();
 
-        const interval = setInterval(() =>
-        {
+        const interval = setInterval(() => {
             const elapsed = Date.now() - start;
             const percentage = Math.max(100 - (elapsed / autoCloseTimer) * 100, 0);
             setProgress(percentage);
         }, intervalDelay);
 
-        const timeout = setTimeout(() =>
-        {
+        const timeout = setTimeout(() => {
             clearInterval(interval);
-            setIsDisplayed(false);
+            handleClose();
         }, autoCloseTimer + 100);
 
         return () => clearTimeout(timeout);
     }, [autoCloseTimer]);
 
-    Snackbar.PropTypes = {
-        type: PropTypes.oneOf(Object.keys(snackBarTypes)),
-        message: PropTypes.string.isRequired,
-        autoCloseTimer: PropTypes.number,
-        className: PropTypes.string
-    }
 
     return (
         isDisplayed && (
             <article
                 title="Cliquer pour fermer" // TODO : Use i18n
-                onClick={() => setIsDisplayed(false)}
+                onClick={handleClose}
                 className={clsx(
-                    "fixed bottom-4 right-1/2 translate-x-1/2 flex flex-col gap-2 w-max max-w-[95%] border-2 rounded-lg hover:cursor-pointer z-[9999]",
+                    `fixed bottom-4
+                     right-1/2
+                     translate-x-1/2
+                     flex flex-col
+                     gap-2
+                     w-max
+                     max-w-[95%]
+                     border-2
+                     rounded-lg
+                     hover:cursor-pointer
+                     z-[9999]
+                     transition transform duration-300
+                     animate-fade-in
+                     shadow-lg/22
+                     hover:shadow-lg/14
+                     active:shadow-lg/22`,
+                    isClosing && "animate-fade-out",
                     snackBarTypes[type].backgroundColor,
                     snackBarTypes[type].borderColor,
                     snackBarTypes[type].color,
@@ -98,11 +111,11 @@ const Snackbar = ({
                                 name={snackBarTypes[type].icon}
                                 color={snackBarTypes[type].color}
                                 size={8}
+                                disableAnimation={true}
                             />
                         </span>
                     }
-
-                    <p className="self-center break-word">
+                    <p className="self-center break-words">
                         {message}
                     </p>
                 </div>
@@ -116,6 +129,13 @@ const Snackbar = ({
             </article>
         )
     )
+}
+
+Snackbar.propTypes = {
+    type: PropTypes.oneOf(Object.keys(snackBarTypes)),
+    message: PropTypes.string.isRequired,
+    autoCloseTimer: PropTypes.number,
+    className: PropTypes.string
 }
 
 export default Snackbar;

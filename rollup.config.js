de5e057aa7d50a4a40065b044aaff2377bd8f2ea
YohanKoch <162994941+YohanKoch@users.noreply.github.com>
2026-08-25 15:03:13 +0200

    hotfix: add react-router-dom as a peer dependency to package.json and rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -68,7 +68,7 @@ function inlineCssIcons(options = {}) {
 
 export default {
   input: 'src/index.js',
-  external: ['react', 'react-dom'],
+  external: ['react', 'react-dom', 'react-router-dom'],
   output: [
     { file: 'dist/index.esm.js', format: 'esm', sourcemap: true },
     { file: 'dist/index.cjs.js', format: 'cjs', sourcemap: true },
